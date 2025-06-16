aa6daa9bfe7756d014ec01989f3c002fb2999fb0
Devonnnnnnnnnn <devonhandler@outlook.com>
2025-06-16 18:00:11 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Login</title>
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <h2>Login</h2>
+  <form id="loginForm">
+    <input type="text" id="userOrEmail" placeholder="Username or Email" required /><br>
+    <input type="password" id="password" placeholder="Password" required /><br>
+    <button type="submit">Login</button>
+    <div id="error"></div>
+  </form>
+  <a href="signup.html">Sign Up</a>
+  <script src="main.js"></script>
+</body>
+</html>
